4371aa7167893abaabe2867ff193e231b18000e7
patryk-kluzik <patrykkluzik99@gmail.com>
2022-04-12 15:32:24 +0100

    sends SMS using twilio API

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import requests
+import datetime
+from twilio.rest import Client
+
+TWILIO_NUMBER = '+12183001930'
+MY_NUMBER = "+447979206765"
+
+account_sid = 'ACe5a2486de26d0a775ae49262e63ff9db'
+auth_token = 'ab136327f0588021748e01aa1341c229'
 
 API_KEY = "a0468448a07475040abfad57749f4a33"
 api_call = "https://api.openweathermap.org/data/2.5/onecall"
 my_lat = 52.399137
 my_lon = -1.520814
 exclude = "current,minutely,daily"
-
-will_rain = None
 
 parameters = {
     "lat": my_lat,
@@ -23,14 +29,26 @@ hourly_data_12_hours = data["hourly"][:12]
 
 weather_codes_for_12_hours = [codes["weather"][0] for codes in hourly_data_12_hours]
 
+current_hour = datetime.datetime.now().hour
 when_to_bring_umbrella = {}
 for index, hour_data in enumerate(weather_codes_for_12_hours):
     if hour_data["id"] < 600:
         bring_umbrella = True
-        when_to_bring_umbrella[index] = hour_data["id"]
+        when_to_bring_umbrella[str(index + current_hour) + ":00"] = hour_data["id"]
+
+list_hours = list(when_to_bring_umbrella.keys())
+
+hours_str = ', '.join(list_hours)
+
+last_char_index = hours_str.rfind(",")
+hours_str = hours_str[:last_char_index] + " and" + hours_str[last_char_index + 1:]
 
 if when_to_bring_umbrella:
-    print("it's going raining")
-    print(list(when_to_bring_umbrella.keys()))
-else:
-    print("it isn't going to rain")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body=f"It's going to rain today between {hours_str}!☂️",
+        from_=TWILIO_NUMBER,
+        to=MY_NUMBER
+    )
+    print(message.status)
